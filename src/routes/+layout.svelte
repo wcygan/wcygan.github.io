20d781bca7deb6d19c45523d9dea85bb56392e5e
Will Cygan <wcygan.io@gmail.com>
2025-01-12 13:59:29 -0600

    wider layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 
 <div class="min-h-screen px-4 py-8">
     <Navbar />
-    <main class="max-w-3xl mx-auto border-2 border-white rounded-lg p-8">
+    <main class="max-w-3xl md:max-w-4xl mx-auto border-2 border-white rounded-lg p-8">
         {@render children()}
     </main>
 </div>
