23b4b270a26dc161a376e33170c119a6d96af417
Will Cygan <wcygan.io@gmail.com>
2025-05-14 17:35:27 -0500

    delete stuff

--- src/routes/resume/+page.svelte
+++
@@ -1,8 +0,0 @@
-<div class="max-w-2xl mx-auto">
-    <h1 class="text-4xl font-bold mb-6">Resume</h1>
-    <div class="prose prose-invert">
-        <p class="mb-4">
-            Professional experience and skills will be listed here.
-        </p>
-    </div>
-</div>
